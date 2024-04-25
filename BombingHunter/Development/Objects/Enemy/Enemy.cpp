ccc5afb1dd3a6870cadf8eeaeb3bd5dc6bc908b3
tokumotoshotaro001 <138739547+tokumotoshotaro001@users.noreply.github.com>
2024-04-25 12:11:34 +0900

    test

---
+++ BombingHunter/Development/Objects/Enemy/Enemy.cpp
@@ -0,0 +1,154 @@
+//#include "Enemy.h"
+//#include "DxLib.h"
+//
+//Enemy::Enemy() :animation_count(0), direction(0.0f)
+//{
+//	animation[0] = NULL;
+//	animation[1] = NULL;
+//}
+//
+//Enemy::~Enemy()
+//{
+//
+//}
+//
+////初期化処理
+//void Enemy::Initialize()
+//{
+//	//画像の読み込み
+//	animation[0] = LoadGraph("Resource/Images/ハコテキ1.png");
+//	animation[1] = LoadGraph("Resource/Images/ハコテキ2.png");
+//
+//	//エラーチェック
+//	if (animation[0] == -1 || animation[1] == -1)
+//	{
+//		throw()
+//	}
+//}
+
+#include "Enemy.h"
+#include "../../Utility/InputControl.h"
+#include "DxLib.h"
+
+//コンストラクタ
+Enemy::Enemy() : animation_count(0), filp_flag(FALSE)
+{
+	animation[0] = NULL;
+	animation[1] = NULL;
+}
+
+//デストラクタ
+Enemy::~Enemy()
+{
+
+}
+
+//初期化処理
+void Enemy::Initialize()
+{
+	//画像の読み込み
+	animation[0] = LoadGraph("Resource/Images/ハコテキ1.png");
+	animation[1] = LoadGraph("Resource/Images/ハコテキ2.png");
+
+	//エラーチェック
+	if (animation[0] == -1 || animation[1] == -1)
+	{
+		throw("ハコテキの画像がありません\n");
+	}
+
+	//向きの設定
+	radian = 0.0f;
+
+	//当たり判定の大きさを設定
+	scale = 64.0f;
+
+	//初期画像の設定
+	image = animation[0];
+}
+
+//更新処理
+void Enemy::Update()
+{
+	//移動処理
+	Movement();
+	//アニメーション制御
+	AnimationControl();
+}
+
+//描画処理
+void Enemy::Draw() const
+{
+	//プレイヤー画像の描画
+	DrawRotaGraphF(location.x, location.y, 1.0, radian, image, TRUE, filp_flag);
+
+	//デバッグ用
+#if _DEBUG
+	//当たり判定の可視化
+	Vector2D ul = location - (scale / 2.0f);
+	Vector2D br = location + (scale / 2.0f);
+	DrawBoxAA(ul.x, ul.y, br.x, br.y, GetColor(255, 0, 0), FALSE);
+#endif
+}
+
+//終了時処理
+void Enemy::Finalize()
+{
+	//使用した画像を解放する
+	DeleteGraph(animation[0]);
+	DeleteGraph(animation[1]);
+}
+
+//当たり判定通知処理
+void Enemy::OnHitCollision(GameObject* hit_object)
+{
+	//当たった時の処理
+}
+
+//移動処理
+void Enemy::Movement()
+{
+	//移動の速さ
+	Vector2D velocity = 0.0f;
+
+	//左右の速さ
+	if (InputControl::GetKey(KEY_INPUT_LEFT))
+	{
+		velocity.x += -1.0f;
+		filp_flag = TRUE;
+	}
+	else if (InputControl::GetKey(KEY_INPUT_RIGHT))
+	{
+		velocity.x += 1.0f;
+		filp_flag = FALSE;
+	}
+	else
+	{
+		velocity.x += 0.0f;
+	}
+	//現在の位置座標に速さを加算する
+	location += velocity;
+}
+
+//アニメーション制御
+void Enemy::AnimationControl()
+{
+	//フレームカウントを加算する
+	animation_count++;
+
+	//60フレーム目に到達したら
+	if (animation_count >= 60)
+	{
+		//カウントのリセット
+		animation_count = 0;
+
+		//画像の切り替え
+		if (image == animation[0])
+		{
+			image = animation[1];
+		}
+		else
+		{
+			image = animation[0];
+		}
+	}
+}
